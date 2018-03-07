78f218feaad54d46696a1a147a020d9a82913f69
AlisonGonzalez <alisongonzalez@outlook.com>
2018-03-06 21:50:19 -0600

    Datos estáticos en tabs

--- app/src/main/java/com/pm/permitmanager/PeriodFragment.java
+++ app/src/main/java/com/pm/permitmanager/PeriodFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -23,6 +25,14 @@ public class PeriodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_period, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.periodList);
+        String[] content = {
+                "Últimos 3 días: 15 permisos vendidos",
+                "Esta semana: 20 permisos vendidos",
+                "Este mes: 27 permisos vendidos"
+        };
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, content);
+        listView.setAdapter(arrayAdapter);
         return view;
     }
 
